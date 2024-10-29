c805036dd3109512bb7c2e8ee56abf699c74be14
entiendoNull <jacobnclausen@gmail.com>
2024-10-29 22:57:43 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
+
+
+function Feed() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up Feed to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function Article() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up Article to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Feed" component={Feed} />
+        <Drawer.Screen name="Article" component={Article} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
